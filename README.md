421b68b221cd86cbb356da8640572c1f62c75e2d
Doctor-Mavix <60850292+Doctor-Mavix@users.noreply.github.com>
2021-06-04 10:57:48 +0200

    vuetify youtube

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# vue-vuetify-youtube-clone
- youtube clone
+# VueTube Vue Frontend - YouTube Clone
+
+> This is the frontend (VueJS) of the VueTube clone
+
+## API
+
+## Features
+
+- Sign in / Sign Up to create channel
+- Video
+  - Upload video
+  - Upload video thumbnail
+  - Watch video
+  - Increase Views
+  - Like and dislike video
+  - Download video
+  - Comment & reply for video
+  - Update video details
+  - Delete video
+- Subscribe to a channel
+- View liked videos
+- Trending
+- Subscriptions
+- History
+  - Watch history
+  - Search history
+- Settings
+  - Modify channel name and email
+  - Change password
+  - Upload channel avatar
+
+## Project setup
+
+Create .env.development.local for development then .env.production.local for production ready app.
+Then put in your api URL
+
+```
+VUE_APP_URL=http://localhost:3001
+```
+
+### Install packages
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```
+npm run build
+```
+
+### Lints and fixes files
+
+```
+npm run lint
+```
+
+### Customize configuration
+
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/router/index.js
@@ -0,0 +1,121 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import NavBar from '@/components/NavBar.vue'
+import StudioNavBar from '@/components/StudioNavBar.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    components: {
+      NavBar,
+      default: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
+    }
+  },
+  {
+    path: '/signin',
+    name: 'SignIn',
+    component: () =>
+      import(/* webpackChunkName: "signin" */ '../views/Auth/SignIn.vue')
+  },
+  {
+    path: '/signup',
+    name: 'SignUp',
+    component: () =>
+      import(/* webpackChunkName: "signup" */ '../views/Auth/SignUp.vue')
+  },
+  {
+    path: '/trending',
+    name: 'Trending',
+    components: {
+      NavBar,
+      default: () =>
+        import(/* webpackChunkName: "trending" */ '../views/Trending.vue')
+    }
+  },
+  {
+    path: '/studio',
+    components: {
+      StudioNavBar,
+      default: () =>
+        import(/* webpackChunkName: "dashboard" */ '../views/Studio/Index.vue')
+    },
+    children: [
+      {
+        path: '/',
+        name: 'Dashboard',
+        component: () =>
+          import(
+            /* webpackChunkName: "dashboard" */ '../views/Studio/Dashboard.vue'
+          )
+      },
+      {
+        path: 'videos',
+        name: 'Video',
+        component: () =>
+          import(/* webpackChunkName: "video" */ '../views/Studio/Video.vue')
+      },
+      {
+        path: 'details/:id',
+        name: 'Detail',
+        component: () =>
+          import(/* webpackChunkName: "video" */ '../views/Studio/Details.vue')
+      }
+    ]
+  },
+  {
+    path: '/channels/:id',
+    components: {
+      NavBar,
+      default: () =>
+        import(/* webpackChunkName: "dashboard" */ '../views/Channel/Index.vue')
+    },
+    children: [
+      {
+        path: '/',
+        name: 'ChannelHome',
+        component: () =>
+          import(
+            /* webpackChunkName: "dashboard" */ '../views/Channel/Home.vue'
+          )
+      }
+    ]
+  },
+  {
+    path: '/watch/:id',
+    name: 'Watch',
+    components: {
+      NavBar,
+      default: () =>
+        import(/* webpackChunkName: "video" */ '../views/Watch.vue')
+    }
+  },
+  {
+    path: '/history',
+    name: 'History',
+    components: {
+      NavBar,
+      default: () =>
+        import(/* webpackChunkName: "video" */ '../views/History.vue')
+    }
+  },
+  {
+    path: '/search',
+    name: 'Search',
+    components: {
+      NavBar,
+      default: () =>
+        import(/* webpackChunkName: "video" */ '../views/Search.vue')
+    }
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
